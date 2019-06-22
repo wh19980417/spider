e60dd46135a679ba50786cde132147fdf7bb3242
wh19980417 <51936149+wh19980417@users.noreply.github.com>
2019-06-22 17:46:51 +0800

    Add files via upload

---
+++ day034.py
@@ -0,0 +1,19 @@
+import requests
+url = 'http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n02127808'
+response = requests.get(url)
+HTML = response.text
+print(HTML)
+URLS = HTML.splist('\n')
+for url in URLS:
+    name = url.split('/')[-1]
+    response = requests.get(url)
+    content = response.content
+    with open(name,'wb') as f:
+       f.write(content)
+    break
+
+
+
+
+
+
